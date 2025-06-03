dc24a47572498402f39575d195a99a0837706102
Lucas Angeli <32561741+Luaxlz@users.noreply.github.com>
2025-06-02 21:59:17 -0300

    ferat(platforms): added the platform controller
    Added new Platform Controller and ger route, refactores the Program.cs

--- Data/PrepDb.cs
+++ Data/PrepDb.cs
@@ -17,9 +17,9 @@ namespace PlatformService.Data
                 Console.WriteLine("--> Seeding data...");
 
                 context.Platforms.AddRange(
-                    new Platform() { Name = "Dot Net", Publisher = "Microsoft", Cost = "Free" },
-                    new Platform() { Name = "SQL Server Express", Publisher = "Microsoft", Cost = "Free" },
-                    new Platform() { Name = "Kubernetes", Publisher = "Cloud Native Computing Foundation", Cost = "Free" }
+                    new Platform() { Id = 1, Name = "Dot Net", Publisher = "Microsoft", Cost = "Free" },
+                    new Platform() { Id = 2, Name = "SQL Server Express", Publisher = "Microsoft", Cost = "Free" },
+                    new Platform() { Id = 3, Name = "Kubernetes", Publisher = "Cloud Native Computing Foundation", Cost = "Free" }
                 );
 
                 context.SaveChanges();
--- Program.cs
+++ Program.cs
@@ -3,49 +3,34 @@ using PlatformService.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
+// Add services to the container
+builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// Database configuration
 builder.Services.AddDbContext<AppDbContext>(opt =>
-    opt.UseInMemoryDatabase("InMem")
-);
+    opt.UseInMemoryDatabase("InMem"));
+
+// Repository and AutoMapper
 builder.Services.AddScoped<IPlatformRepo, PlatformRepo>();
 builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 
 var app = builder.Build();
 
+// Configure the HTTP request pipeline
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
 
-app.UseHttpsRedirection();
+// app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
+// IMPORTANTE: Mapear os controllers
+app.MapControllers();
 
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast = Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast")
-.WithOpenApi();
-
+// Preparar o banco de dados
 PrepDb.PrepPopulation(app);
 
 app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
