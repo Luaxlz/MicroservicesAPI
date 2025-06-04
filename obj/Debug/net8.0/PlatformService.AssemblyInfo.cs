d3b6700b2f62e9e7cbcb6b611ccf579e7008c9e6
Lucas Angeli <32561741+Luaxlz@users.noreply.github.com>
2025-06-03 22:16:21 -0300

    feat(infra): Added a dockerfile to initialize the container

--- obj/Debug/net8.0/PlatformService.AssemblyInfo.cs
+++ obj/Debug/net8.0/PlatformService.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PlatformService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f019116dd7453a4a02b5c60059ba10c3ccbcdbfd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+09bd042dc48b93224f561aff74846ec48e4cf4e2")]
 [assembly: System.Reflection.AssemblyProductAttribute("PlatformService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PlatformService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
